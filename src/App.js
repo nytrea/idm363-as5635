122c31eedfe241a145ca4b289c754cea18515905
nytrea <72425215+nytrea@users.noreply.github.com>
2022-11-01 11:16:10 -0500

    Commit Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+//import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      <h1> Muji </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Nav, Navbar as NavbarBoot } from 'react-bootstrap';
+
+export function Navbar() {
+   
+}
